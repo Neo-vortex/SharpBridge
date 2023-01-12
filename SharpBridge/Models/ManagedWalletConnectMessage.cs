634e2f6eccc44174af427715303a09e872b9c240
Mohamadrza Nakhleh <neo.vortex@pm.me>
2023-01-12 09:37:47 +0330

    (improve) 1-logging

--- SharpBridge/Models/ManagedWalletConnectMessage.cs
+++ SharpBridge/Models/ManagedWalletConnectMessage.cs
@@ -2,8 +2,18 @@ namespace SharpBridge.Models;
 
 public class ManagedWalletConnectMessage
 {
+    public ManagedWalletConnectMessage()
+    {
+        CreationTime = DateTime.UtcNow;
+        ID = Guid.NewGuid();
+    }
+
     public  WalletConnectMessage Message { get; set; }
     public byte[] Binary { get; set; }
     
+    private DateTime CreationTime { get; set; }
     
+    public bool Expired => DateTime.UtcNow - CreationTime > TimeSpan.FromMinutes(5);
+    
+    public Guid ID { get; set; }
 }
--- SharpBridge/Services/Dispatcher.cs
+++ SharpBridge/Services/Dispatcher.cs
@@ -11,8 +11,10 @@ public class Dispatcher
     private  readonly ConcurrentDictionary< Guid ,ManagedWalletConnectMessage> ManagedWalletConnectMessages = new();
 
 
-    public Dispatcher()
+    private readonly ILogger<Dispatcher> _logger;
+    public Dispatcher(ILogger<Dispatcher> logger)
     {
+        _logger = logger;
         new Thread(Committer).Start();
     }
 
@@ -21,9 +23,9 @@ public class Dispatcher
         while (true)
         {
             foreach (var message in ManagedWalletConnectMessages)
-            {
-               var result =  DispatchMessages(message.Value);
-               if (result)
+            { 
+                var result =  DispatchMessages(message.Value);
+               if (result || message.Value.Expired)
                {
                    ManagedWalletConnectMessages.TryRemove(message.Key,out _);
                }
@@ -34,22 +36,23 @@ public class Dispatcher
 
     public void RemoveWebSocket(Guid ID)
     {
+
         foreach (var clients in ManagedWebSockets)
         {
             foreach (var client in clients.Value.Where(client => client.ID == ID))
             {
                 ManagedWebSockets.TryUpdate(clients.Key, clients.Value.Where(client => client.ID != ID).ToList(),
                     clients.Value);
-                if (ManagedWebSockets[clients.Key].Count == 0)
-                {
-                    ManagedWebSockets.TryRemove(clients.Key, out _);
-                }
+                if (ManagedWebSockets[clients.Key].Count != 0) continue;
+                var result =     ManagedWebSockets.TryRemove(clients.Key, out _);
+                _logger.LogInformation(result ? $"WebSocket {ID} was closed" : $"WebSocket {ID} was failed to closed");
             }
         }
     }
 
     public void AddNewWebSocket(ManagedWebSocket managedWebSocket)
     {
+        _logger.LogInformation( $"New subscriber was added" );
         ManagedWebSockets.AddOrUpdate( managedWebSocket.Topic , guid => new List<ManagedWebSocket>(){managedWebSocket} , (guid, list) =>
         {
             list.Add(managedWebSocket);
@@ -59,7 +62,15 @@ public class Dispatcher
 
     public void AddNewMessage(ManagedWalletConnectMessage message )
     {
-        ManagedWalletConnectMessages.TryAdd( (Guid)message.Message.Topic!, message);
+       var result =  ManagedWalletConnectMessages.TryAdd( message.ID, message);
+       if (result)
+       {
+           _logger.LogInformation( $"New message {message.ID} was added" );
+       }
+       else
+       {
+           _logger.LogWarning( $"Failed to add message {message.ID}" );
+       }
     }
 
     private bool  DispatchMessages(ManagedWalletConnectMessage message)
@@ -76,11 +87,12 @@ public class Dispatcher
                     {
                         clients.WebSocket.SendAsync(new ReadOnlyMemory<byte>((message.Binary)),
                             WebSocketMessageType.Text, true, CancellationToken.None).AsTask().Wait();
+                        _logger.LogInformation( $"Message {message.ID} was delivered" );
                         result = true;
                     }
                     catch (Exception e)
                     {
-                        continue;
+                        _logger.LogWarning($"Failed to write message  {message.ID} to websocket pipeline." + Environment.NewLine + e.Message);
                     }
                 }
             });
@@ -100,7 +112,7 @@ public class Dispatcher
                     }
                     catch (Exception e)
                     {
-                        continue;
+                        _logger.LogWarning("Failed to write message to websocket pipeline." + Environment.NewLine + e.Message);
                     }
                 }
             }
